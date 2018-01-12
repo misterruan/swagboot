5ebfa8a33ba0af25edc41b4526964213798918a9
ruanqiangsong <misterruan@163.com>
2018-01-12 09:56:32 +0800

    delete swagger static files

--- src/main/java/com/hl/App.java
+++ src/main/java/com/hl/App.java
@@ -9,6 +9,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 //@ComponentScan
 //@Configuration
@@ -17,7 +19,11 @@ public class App {
 	private static Logger logger = LoggerFactory.getLogger(App.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
+		String[] activeProfiles = SpringApplication.run(App.class, args)
+				.getEnvironment().getActiveProfiles();
+		Arrays.stream(activeProfiles).forEach(profile->{
+			logger.warn("Spring Boot 使用profile为:{}" , profile);
+		});
 		logger.info("SpringBoot Start Success");
 	}
 }
